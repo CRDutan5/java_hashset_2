56f3c56a88a08e503a9438eec0313112aba2eec4
Carlitos Dutan <carlitosdutan@pursuit.org>
2025-04-16 16:28:57 -0400

    completed exam

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -4,4 +4,14 @@ import java.util.Objects; // For equals() and hashCode()
 
 public class Car {
 
+    String model;
+    String brand;
+
+    public Car(String model, String brand) {
+        this.model = model;
+        this.brand = brand;
+    }
+
+
+
 }
--- src/main/java/org/example/CarService.java
+++ src/main/java/org/example/CarService.java
@@ -5,5 +5,45 @@ import java.util.Set;
 
 public class CarService {
 
+    Set<Car> carSet = new HashSet<>();
+    int carCount = 0;
+
+    public void addCar(Car car) {
+        if (car == null) {
+            throw new IllegalArgumentException();
+        }
+        carSet.add(car);
+        carCount++;
+    }
+
+    public boolean removeCar(Car car) {
+        if (carSet.contains(car)) {
+            carSet.remove(car);
+            carCount--;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getCarCount() {
+        return carCount;
+    }
+
+    public Set<Car> getCarsByMake(String make) {
+        Set<Car> cars = new HashSet<>();
+
+        if (make == null || make.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Car car : carSet) {
+            if (car.brand.equalsIgnoreCase(make)) {
+                cars.add(car);
+            }
+        }
+        return cars;
+    }
 
 }
